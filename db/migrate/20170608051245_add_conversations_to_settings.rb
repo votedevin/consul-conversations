a84edc0018d53056c157b46f7078409bceab2e0e
Semyon Pupkov <mail@semyonpupkov.com>
2017-06-12 00:30:09 +0500

    Start conversation button url from settings

--- db/migrate/20170608051245_add_conversations_to_settings.rb
+++ db/migrate/20170608051245_add_conversations_to_settings.rb
@@ -3,6 +3,7 @@ class AddConversationsToSettings < ActiveRecord::Migration
     execute <<-SQL
       INSERT INTO settings (key, value) VALUES ('feature.conversations', 'true');
       INSERT INTO settings (key, value) VALUES ('main_conversations_rss_url', '');
+      INSERT INTO settings (key, value) VALUES ('start_conversation_url', '#');
     SQL
   end
 end
